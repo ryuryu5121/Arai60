a38d86a91ed4b876d6f5c57ba3d1a39eae5ee27c
ryuryu5121 <r.oshima.12.15@gmail.com>
2024-12-31 15:57:40 +0900

    submit Linked_List_Cycle_2

--- Linked_List_Cycle_2/step3.py
+++ Linked_List_Cycle_2/step3.py
@@ -1,28 +1,27 @@
-
-# LeetCodeの回答を基に修正, tortise and hare
 class Solution(object):
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # あえて2行に分けることで可読性アップ
-        slow = fast
         fast = head
-        while fast is not None and fast.next is not None:
-            slow = slow.next
+        slow = slow
+
+        while slow is not None and fast.next is not None:
             fast = fast.next.next
-            if slow == fast: 
+            slow = slow.next.next
+
+            if fast == slow:
                 break
-        else: 
-            return None  # if not (fast and fast.next): return None
+        else:
+            return None
         
         origin = head
         while origin != slow:
             origin = origin.next
             slow = slow.next
         return origin
-
-# set()を使ったバージョンの修正
+    
 def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
     visited_nodes = set()
     node = head
+
     while node is not None:
         if node in visited_nodes:
             return node
@@ -30,4 +29,3 @@ def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         visited_nodes.add(node)
         node = node.next
     return None
-
